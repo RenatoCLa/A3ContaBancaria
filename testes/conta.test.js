d2589eedb6614de4fdfe8b81af3f224361e8eed1
Renato Costa <xmassiveg@gmail.com>
2024-04-11 09:31:32 -0300

    Adicionado um titulo para os testes

--- testes/conta.test.js
+++ testes/conta.test.js
@@ -4,32 +4,32 @@ const Pessoa = require("../pessoa.js");
 describe('Testes da classe Conta', () =>{
     
     //Variáveis da classe conta na inicialização - fabio
-    it('', () =>{
-
+    it('Deve apresentar as variáveis certas quando criado', () =>{
+        
     });
 
     //Depositar um valor positivo a uma conta - gusta
-    it('', () =>{
+    it('Deve depositar um valor positivo a outra conta', () =>{
         
     });
 
     //Receber transferencia - gusta
-    it('', () =>{
+    it('Deve receber um valor e adiciona-lo ao saldo', () =>{
         
     });
 
     //Sacar um valor positivo - gusta
-    it('', () =>{
+    it('Deve sacar um valor positivo do seu saldo', () =>{
         
     });
 
     //Transferir um valor positivo a uma conta - gusta
-    it('', () =>{
+    it('Deve transferir um valor positivo a outra conta', () =>{
         
     });
 
     //Ver o saldo da conta - gusta
-    it('', () =>{
+    it('Deve mostrar corretamente o saldo da conta', () =>{
         
     });
 });
@@ -37,52 +37,52 @@ describe('Testes da classe Conta', () =>{
 describe('Testes de erro da classe Conta', () =>{
 
     //Depositar um valor negativo - dani
-    it('', () =>{
+    it('Não deve permitir o deposito de um valor negativo', () =>{
         
     });
     
     //Depositar um valor 0 - dani
-    it('', () =>{
+    it('Não deve permitir o deposito de 0', () =>{
         
     });
 
     //Depositar um valor maior do que o seu limite - dani
-    it('', () =>{
+    it('Não deve permitir o deposito de um valor acima do dinheiro que a pessoa tem em mãos', () =>{
         
     });
 
     //Sacar um valor negativo - dani
-    it('', () =>{
+    it('Não deve permitir sacar um valor negativo', () =>{
         
     });
 
     //Sacar um valor 0 - dani
-    it('', () =>{
+    it('Não deve permitir sacar um valor 0', () =>{
         
     });
 
     //Sacar um valor maior que o seu saldo - edu
-    it('', () =>{
+    it('Não deve permitir sacar um valor que excede o seu saldo', () =>{
         
     });
 
     //Transferir para a sua propria conta - edu
-    it('', () =>{
+    it('Não deve permitir a transferencia para a sua própria conta', () =>{
         
     });
 
     //Transferir um valor negativo - edu
-    it('', () =>{
+    it('Não deve permitir a transferencia de um valor negativo', () =>{
         
     });
 
     //Transferir um valor 0 - edu
-    it('', () =>{
+    it('Não deve permitir a transferencia de um valor 0', () =>{
         
     });
 
     //Transferir um valor maior que o seu saldo - edu
-    it('', () =>{
+    it('Não deve permitir a transferencia de um valor que excede o seu saldo', () =>{
         
     });
 });
--- testes/pessoa.test.js
+++ testes/pessoa.test.js
@@ -3,22 +3,22 @@ const Pessoa = require("../pessoa.js");
 describe('Teste da classe pessoa', () =>{
 
     //Variáveis da classe presentes na inicialização - fabio
-    it('', () =>{
+    it('Deve apresentar corretamente as variáveis recebidas na sua criação', () =>{
         
     });
     
     //Realizar deposito - fabio
-    it('', () =>{
+    it('Deve realizar o deposito para sua conta', () =>{
         
     });
 
     //Receber - fabio
-    it('', () =>{
+    it('Deve receber o valor de um saque', () =>{
         
     });
 
     //Ver dinheiro - fabio
-    it('', () =>{
+    it('Deve visualizar corretamente o dinheiro da pessoa', () =>{
         
     });
 });
