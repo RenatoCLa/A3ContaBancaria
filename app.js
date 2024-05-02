53047a9b28f126e03e14f8f8e36438fba56449bb
Renato Costa <xmassiveg@gmail.com>
2024-05-02 00:04:30 -0300

    Adicionado funções CRUD

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
-const Conta = require("./conta");
+const express = require('express');
+const app = express();
+const Conta = require('./conta');
 
-//Solução temporária para a criação de IDs de conta
-const conta01 = new Conta("joao", (Math.random() * 100000000).toFixed(0), 1012123109, 10);
+app.use(express.json());
 
-const conta02 = new Conta("jorge", (Math.random() * 100000000).toFixed(0), 23210321321, 0);
+//CRUD
 
-conta01.verSaldo();
+//CREATE (feito)
+//READ (feito)
+//UPDATE (feito)
+//DELETE
 
-conta01.sacar(4);
+//array para armazenamento de contas, enquanto o banco de dados não é implementado
+let contas = [];
 
-conta01.verSaldo();
+//pagina inicial
+app.get('/', (req, res) => {
+    res.send("The website is up");
+});
 
-conta01.depositar(5);
+//cria uma nova conta
+app.post('/conta', (req, res) =>{
+    const conta = Conta.create(contas, req.body);
+    res.status(201).json(Conta.returnConta(conta));
+})
 
-conta01.verSaldo();
+//visualizar todas as contas
+app.get('/conta', (req, res) =>{
+    const listaContas = Conta.verTodasContas(contas);
+    res.send(listaContas);
+});
 
-conta02.verSaldo();
+//atualizar informações de uma conta
+app.put('/conta/:id', (req, res) =>{
+    const contaAtualizada = Conta.update(contas, parseInt(req.params.id), req.body);
+    if (contaAtualizada){
+        res.json(Conta.returnConta(contaAtualizada));
+    }
+    else{
+        res.status(404).send('Conta não foi encontrada');
+    }
+});
 
-conta01.transferir(10, conta02);
+//deleta uma conta
+app.delete('/conta/:id', (req, res) =>{
+    const deletado = Conta.delete(contas, parseInt(req.params.id));
+    if (deletado) {
+        res.status(204).send('Conta apagada');
+    }
+    else{
+        res.status(404).send('Conta não foi encontrada');
+    }
+});
 
-conta01.verSaldo();
-
-conta02.verSaldo();
+//inicia o servidor na porta 3000
+app.listen(3000, () => {
+    console.log("Server has started on port:3000");
+});
--- conta.js
+++ conta.js
@@ -1,102 +1,92 @@
 class Conta {
 
-    constructor(nome, ID, cpf, saldo){
+    constructor(id, cpf, nome, saldo){
         this.nome = nome;
         this.saldo = saldo;
         this.cpf = cpf;
-        this.ID = ID;
+        this.id = id;
     }
-
-    /* Esperar a criação do banco de dados para implementar essa função
-    gerarID(){
-
-    }
-    */
-
-    //Notas | Todo
 
     //Converter os valores imprimidos para valores monetarios; ex: 4 => 04,00
     //Implementar a mudança do saldo da conta no banco de dados em todas as funções
 
+    //funções crud
+
+    //retorna os dados de uma conta, para que possa ser exibida após um request
+    static returnConta(conta){
+        return{
+            id: conta.id,
+            cpf: conta.cpf,
+            nome: conta.nome,
+            saldo: conta.saldo
+        }
+    }
+
+    //função statica para criar uma conta bancária
+    static create(contas, data){
+        const createdConta = new Conta(contas.length +1, data.cpf, data.nome, data.saldo);
+        contas.push(createdConta);
+        return this.returnConta(createdConta);
+    }
+
+    //retorna todas as contas presentes no sistema
+    static verTodasContas(contas){
+        return contas.map(conta => this.returnConta(conta));
+    }
+
+    //retorna a busca de uma conta, baseada em seu id
+    static buscarPorid(contas, id){
+        return contas.find(conta => conta.id === id);
+    };
+
+    //atualiza as informações de uma conta
+    static update(contas, id, data){
+        const conta = this.buscarPorid(contas, id);
+        if (conta) {
+            conta.cpf = data.cpf || conta.cpf;
+            conta.nome = data.nome || conta.nome;
+            conta.saldo = data.saldo || conta.saldo;
+        }
+        return conta;
+    };
+
+    //deleta uma conta
+    static delete(contas, id){
+        const deletarConta = this.buscarPorid(contas, id);
+        if (deletarConta) {
+            const index = contas.indexOf(deletarConta);
+            contas.splice(index, 1);
+            return true;
+        }
+        return false;
+    };
+
+    //funções
+
     sacar(valor){
         this.saldo -= valor;
-        console.log("Conta " + this.ID + " sacou : R$" + valor + "\n");
+        console.log("Conta " + this.id + " sacou : R$" + valor + "\n");
     }
 
     depositar(valor){
         this.saldo += valor;
-        console.log("Conta " + this.ID + " depositou : R$" + valor + "\n");
+        console.log("Conta " + this.id + " depositou : R$" + valor + "\n");
     }
 
     transferir(valor, conta){
         this.saldo -= valor;
-        console.log("Realizado transferência de R$ " + valor + " da conta " + this.ID + " para a conta " + conta.ID + "\n");
+        console.log("Realizado transferência de R$ " + valor + " da conta " + this.id + " para a conta " + conta.id + "\n");
         conta.receberTransferencia(valor);
     }
 
     receberTransferencia(valor){
         this.saldo += valor;
-        console.log("Conta " + this.ID + " recebeu transferência de R$" + valor + "\n");
+        console.log("Conta " + this.id + " recebeu transferência de R$" + valor + "\n");
     }
 
     verSaldo(){
-        console.log("O saldo da conta " + this.ID + " é : R$" + this.saldo + "\n");
+        console.log("O saldo da conta " + this.id + " é : R$" + this.saldo + "\n");
     }
-
-    /*
-
-    depositar(deposito){
-        if(deposito <= 0){
-            console.log("numero inválido");
-            throw new Error('numero inválido');
-        }else if(this.pessoa.dinheiro >= deposito){
-            this.saldo += deposito;
-            this.pessoa.realizarDeposito(deposito);
-        }else{
-            console.log("Você não possui dinheiro o suficiente para realizar o deposito.");
-            throw new Error('dinheiro insuficiente');
-        }
-    }
-
-    receberTransferencia(transferencia){
-        this.saldo += transferencia;
-    }
-
-    sacar(valor){
-        if(valor <= 0){
-            console.log("numero inválido");
-            throw new Error('numero inválido');
-        }else if(this.saldo >= valor){
-            this.saldo -= valor;
-            this.pessoa.receber(valor);
-        }else{
-            console.log("Você não possui saldo suficiente.");
-            throw new Error('Você não possui saldo suficiente');
-        }
-    }
-
-    transferir(valor, conta){
-        if(valor <= 0){
-            console.log("numero inválido");
-            throw new Error('numero inválido');
-        }else if(conta.nome === this.nome){
-            console.log("Não é possivel realizar uma tranferência para a sua própria conta.");
-            throw new Error('transferencia para a própria conta não é permitido');
-        }else if(this.saldo >= valor){
-            this.saldo -= valor;
-            conta.receberTransferencia(valor);
-        }else{
-            console.log("Saldo insuficiente.");
-            throw new Error('saldo insuficiente');
-        }
-    }
-
-    verSaldo(){
-        console.log("Conta: " + this.nome +" Saldo: R$" + this.saldo);
-        return this.saldo;
-    }
-
-    */
 }
 
 module.exports = Conta;
