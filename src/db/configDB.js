edbb8a22638b4395bbd709bc639385841918db0f
FabianoBrisola <132628695+RenatoCLa@users.noreply.github.com>
2024-06-12 12:29:03 -0300

    Alterações no codigo do banco de dados

--- src/db/configDB.js
+++ src/db/configDB.js
@@ -1,22 +1,27 @@
 import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('conta_bancaria', (err) => {
+const db = new sqlite3.Database('conta_bancaria.db', (err) => {
     if (err){
         console.error(err.message);
     }
     else{
         db.get("PRAGMA foreign_keys = ON");
         
-        db.run(`CREATE TABLE IF NOT EXISTS banco (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)`, (err) => {
+        db.run(`CREATE TABLE IF NOT EXISTS banco (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL)`,
+            (err) => {
             if(err){
                 console.error(err.message);
-            }else{
-                console.log("Table exists");
             }
         });
 
-        db.run(`CREATE TABLE IF NOT EXISTS conta (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, cpf INTEGER,
-            saldo INTEGER, banco_id INTEGER, FOREIGN KEY (banco_id) REFERENCES banco (id))`,
+        db.run(`CREATE TABLE IF NOT EXISTS conta (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            cpf INTEGER NOT NULL UNIQUE,
+            saldo INTEGER NOT NULL,
+            banco_id INTEGER NOT NULL,
+            FOREIGN KEY (banco_id) REFERENCES banco (id))`,
             (err) => {
                 if (err){
                     console.error(err.message);
@@ -24,9 +29,8 @@ const db = new sqlite3.Database('conta_bancaria', (err) => {
             }
         );
 
-        db.run(`INSERT INTO banco (nome) VALUES ('Bradesco')`);
-        db.run(`INSERT INTO banco (nome) VALUES ('Caixa')`);
-        db.run(`INSERT INTO banco (nome) VALUES ('Santander')`);
-        db.run(`INSERT INTO banco (nome) VALUES ('Banco do Brasil')`);
+        
     }
 });
+
+export default db;
